7c941c4faa0f14d2d13d69292ee75932c8cbae40
robertobenfini <roberto.benfini@gmail.com>
2023-06-20 13:38:23 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+<template>
+    <div>
+      <h1>header</h1>  
+    </div>
+</template>
+<style lang="">
+    
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+<template lang="">
+    <div>
+        <h1>main</h1>
+    </div>
+</template>
+<style lang="">
+    
+</style>
